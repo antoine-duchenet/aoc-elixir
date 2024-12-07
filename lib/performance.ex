5287a5f06e3f5ca605f5c14d717ce2ea5711d1b3
Antoine Duchenet <antoine.duchenet@gmail.com>
2024-12-07 14:08:51 +0100

    ✨: create the Performance.read_memoized helper

--- lib/performance.ex
+++ lib/performance.ex
@@ -3,6 +3,10 @@ defmodule Performance do
     with nil <- Process.get(key), do: tap(f.(), &Process.put(key, &1))
   end
 
+  def read_memoized(key, f) do
+    with nil <- Process.get(key), do: f.()
+  end
+
   def once(key, otherwise, f) do
     with nil <- Process.get(key) do
       Process.put(key, true)
